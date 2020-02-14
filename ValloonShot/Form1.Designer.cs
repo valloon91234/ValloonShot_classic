d7a1642c007d943506843c2b133fc2d34bfa6e24
Valloon427428 <61017400+valloon91234@users.noreply.github.com>
2020-02-14 22:13:51 +0900

    Initial commit

---
+++ ValloonShot/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace com.valloon.ValloonShot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.openConfigToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openTargetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.openConfigToolStripMenuItem,
+            this.openTargetToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(152, 126);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.startToolStripMenuItem.Text = "&Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.stopToolStripMenuItem.Text = "St&op";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(148, 6);
+            // 
+            // openConfigToolStripMenuItem
+            // 
+            this.openConfigToolStripMenuItem.Name = "openConfigToolStripMenuItem";
+            this.openConfigToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.openConfigToolStripMenuItem.Text = "Open &Config...";
+            this.openConfigToolStripMenuItem.Click += new System.EventHandler(this.openConfigToolStripMenuItem_Click);
+            // 
+            // openTargetToolStripMenuItem
+            // 
+            this.openTargetToolStripMenuItem.Name = "openTargetToolStripMenuItem";
+            this.openTargetToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.openTargetToolStripMenuItem.Text = "Open &Target...";
+            this.openTargetToolStripMenuItem.Click += new System.EventHandler(this.openTargetToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(148, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "ValloonShot";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.Text = "ValloonShot";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem openConfigToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem openTargetToolStripMenuItem;
+    }
+}
+
---
+++ ValloonShot/Form1.cs
@@ -0,0 +1,312 @@
+﻿using com.valloon.ValloonShot.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace com.valloon.ValloonShot
+{
+    public partial class Form1 : Form
+    {
+
+        private String extension = @"png";
+        private readonly String path = Application.StartupPath + @"\";
+        private readonly String path2 = Application.StartupPath + @"\_shot\";
+        private Boolean StealthMode = true;
+        private int interval1 = 10;
+        private int interval2 = 1799;
+        private int hotkey = (int)Keys.Space;
+        private String[] exceptWindowTitles = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static String CorrectFileNames(String input)
+        {
+            String filename = input.Replace('/', ' ').Replace('\\', ' ').Replace(':', '.').Replace('*', ' ').Replace('?', ' ').Replace('\"', ' ').Replace('<', ' ').Replace('>', ' ').Replace('|', ' ');
+            if (filename.Length > 200) filename = filename.Substring(0, 200) + "...";
+
+            //string illegal = "\"M\"\\a/ry/ h**ad:>> a\\/:*?\"| li*tt|le|| la\"mb.?";
+            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            filename = r.Replace(filename, "");
+
+            return filename;
+        }
+
+        [DllImport("User32")]
+        public static extern bool RegisterHotKey(
+            IntPtr hWnd,
+            int id,
+            int fsModifiers,
+            int vk
+        );
+        [DllImport("User32")]
+        public static extern bool UnregisterHotKey(
+            IntPtr hWnd,
+            int id
+        );
+
+        public const int MOD_WIN = 0x8;
+        public const int MOD_SHIFT = 0x4;
+        public const int MOD_CONTROL = 0x2;
+        public const int MOD_ALT = 0x1;
+        public const int WM_HOTKEY = 0x312;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+        private Boolean ExceptThis(String SubActiveWindowTitle)
+        {
+            if (SubActiveWindowTitle == null) return true;
+            foreach (String one in exceptWindowTitles)
+            {
+                if (SubActiveWindowTitle.IndexOf(one, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void ScreenShot(String path, String filename)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            if (!dir.Exists) dir.Create();
+            if (!path.EndsWith(@"\")) path += @"\";
+            filename = path + filename;
+            Size size = SystemInformation.PrimaryMonitorSize;
+            Bitmap bitmap = new Bitmap(size.Width, size.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.CopyFromScreen(0, 0, 0, 0, size);
+            graphics.Flush();
+            bitmap.Save(filename, ImageFormat.Png);
+            bitmap.Dispose();
+            graphics.Dispose();
+        }
+
+        private void hotkeyShot()
+        {
+            DateTime time = DateTime.Now;
+            String filename = time.ToString("yyyy-MM-dd  HH.mm.ss");
+            try
+            {
+                String activeWindowTitle = GetActiveWindowTitle();
+                if (activeWindowTitle != null)
+                {
+                    filename += "  " + CorrectFileNames(activeWindowTitle) + "." + extension;
+                }
+                else
+                {
+                    filename += "." + extension;
+                }
+                ScreenShot(path2, filename);
+                notifyIcon1.ShowBalloonTip(0, "Success !\r\n", filename, ToolTipIcon.Info);
+            }
+            catch (Exception ex)
+            {
+                notifyIcon1.ShowBalloonTip(0, "Save failed !\r\n" + filename, ex.Message, ToolTipIcon.Error);
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY && m.WParam == (IntPtr)0)
+            {
+                hotkeyShot();
+            }
+            base.WndProc(ref m);
+            this.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.Visible = false;
+            if (!RegisterHotKey(this.Handle, 0, MOD_WIN + MOD_ALT, hotkey) && !StealthMode)
+                MessageBox.Show("Set hotkey failed.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            startToolStripMenuItem_Click(sender, e);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime time = DateTime.Now;
+            String filename = time.ToString("yyyy-MM-dd  HH.mm.ss");
+            try
+            {
+                String activeWindowTitle = GetActiveWindowTitle();
+                if (ExceptThis(activeWindowTitle)) return;
+                if (activeWindowTitle != null)
+                {
+                    filename += "  " + CorrectFileNames(activeWindowTitle) + "." + extension;
+                }
+                else
+                {
+                    filename += "." + extension;
+                }
+                ScreenShot(path + time.ToString("yyyy-MM-dd"), filename);
+            }
+            catch (Exception ex)
+            {
+                notifyIcon1.ShowBalloonTip(0, "Save failed !\r\n" + filename, ex.Message, ToolTipIcon.Error);
+                throw ex;
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            DateTime time = DateTime.Now;
+            String filename = time.ToString("yyyy-MM-dd  HH.mm.ss");
+            try
+            {
+                String activeWindowTitle = GetActiveWindowTitle();
+                if (ExceptThis(activeWindowTitle)) return;
+                if (activeWindowTitle != null)
+                {
+                    filename += "  " + CorrectFileNames(activeWindowTitle) + "." + extension;
+                }
+                else
+                {
+                    filename += "." + extension;
+                }
+                ScreenShot(path + time.ToString("yyyy"), filename);
+            }
+            catch (Exception ex)
+            {
+                notifyIcon1.ShowBalloonTip(0, "Save failed !\r\n" + filename, ex.Message, ToolTipIcon.Error);
+            }
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String filename = path + "config.cfg";
+                FileInfo fileInfo = new FileInfo(filename);
+                if (fileInfo.Exists)
+                {
+                    StreamReader reader = File.OpenText(filename);
+                    String line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.StartsWith("//")) continue;
+                        String[] var = line.Split(new Char[] { '=' }, 2);
+                        if (var[0] == "interval1")
+                        {
+                            interval1 = Convert.ToInt16(var[1]);
+                        }
+                        else if (var[0] == "interval2")
+                        {
+                            interval2 = Convert.ToInt16(var[1]);
+                        }
+                        else if (var[0] == "extension")
+                        {
+                            extension = var[1].Trim();
+                        }
+                        else if (var[0] == "expect")
+                        {
+                            exceptWindowTitles = var[1].Trim().Split(new String[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                        }
+                        else if (var[0] == "stealth")
+                        {
+                            StealthMode = var[1].Trim() == "1";
+                            this.notifyIcon1.Visible = !StealthMode;
+                        }
+                    }
+                    reader.Close();
+                    reader.Dispose();
+                }
+                else
+                {
+                    StreamWriter writer = new StreamWriter("config.cfg");
+                    writer.Write(
+@"interval1=10
+interval2=1199
+extension=png
+expect=Program Manager");
+                    writer.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (!StealthMode)
+                    MessageBox.Show("Read config failed. " + ex.Message, "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            notifyIcon1.ShowBalloonTip(0, "I am alive !", "Interval1=" + interval1 + ", Interval2=" + interval2, ToolTipIcon.Info);
+            timer1.Interval = 1000 * interval1;
+            timer2.Interval = 1000 * interval2;
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            notifyIcon1.Icon = Resources.valloon;
+            startToolStripMenuItem.Enabled = false;
+            stopToolStripMenuItem.Enabled = true;
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            //timer2.Enabled=false;
+            notifyIcon1.Icon = Resources.valloon_gray;
+            startToolStripMenuItem.Enabled = true;
+            stopToolStripMenuItem.Enabled = false;
+        }
+
+        private void openConfigToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("Notepad.exe", "config.cfg");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 0);
+            this.Close();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            if (startToolStripMenuItem.Enabled)
+                startToolStripMenuItem_Click(sender, e);
+            else
+                stopToolStripMenuItem_Click(sender, e);
+        }
+
+        private void openTargetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", @"file:///" + path);
+        }
+    }
+}
